966c793afbb8b23a310c3f0bf557b57d73dcc3c4
Jxk0be <JakeShoffner27@gmail.com>
2023-12-31 14:26:32 -0500

    feat: Setting up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1 className='font-bold text-red-500'>Testing TailwindCSS</h1>,
+    errorElement: <h1 className="text-3xl font-bold text-red-400">Page Does Not Exist</h1>
+  },
+])
+
 function App() {
 
   return (
     <>
-      <h1 className='font-bold text-red-500'>Testing TailwindCSS</h1>
+      <Navbar />
+      <div>
+        <RouterProvider router={router} />
+      </div>
     </>
   )
 }
