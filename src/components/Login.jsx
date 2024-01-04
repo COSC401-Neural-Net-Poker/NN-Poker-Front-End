c8587ad42a25c7b94f5e407a241fc05485da39c5
Jxk0be <jakeshoffner27@gmail.com>
2024-01-03 20:26:24 -0500

    Login CSS attempt #1

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { signInWithEmailAndPassword, onAuthStateChanged, signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from '../firebase';
 import { useNavigate } from "react-router-dom";
+import { Icon } from '@iconify/react';
 
 const Login = ({reg}) => {
   const navigate = useNavigate();
@@ -35,13 +36,20 @@ const Login = ({reg}) => {
   }
 
   return (
-    <div className="bg-gray-300 w-1/2 h-1/2">
-      <form onSubmit={(e) => handleLogin(e)}>
-        <input placeholder="Email" type="text" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-        <input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-        <button type="submit" className="bg-sky-400 py-1 px-2 rounded-xl">Login</button>
+    <div className="bg-gray-300 w-1/2 h-1/2 flex justify-center flex-col items-center text-[#4B4B4B]">
+      <div className="flex justify-center items-center drop-shadow-md border-b-2 w-1/2 border-[#adb0ae]">
+        <Icon className="text-[45px]" icon="ph:user-fill" />
+        <h1 className="text-[40px] font-bold">Login</h1>
+      </div>
+
+      <form className="mt-2 flex flex-col items-center w-1/2" onSubmit={(e) => handleLogin(e)}>
+        <input className="mb-4 w-full rounded-md pl-1 text-[20px]" placeholder="Email" type="text" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+        <input className="mb-4 w-full rounded-md pl-1 text-[20px]" placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+        <button className="mb-4 rounded-md w-full py-1 font-semibold bg-[#4B4B4B] text-white" type="submit">Login</button>
       </form>
-      <button onClick={() => handleGoogle()} className="bg-sky-400 py-1 px-2 rounded-xl">Sign in with google</button>
+      <button onClick={() => handleGoogle()} className="bg-[#fef9f3] mb-5 w-1/2 duration-150 hover:bg-[#FF8200] hover:text-white flex justify-center items-center py-1 px-2 rounded-xl">
+        <Icon className="mr-3 text-[30px]" icon="flat-color-icons:google" />Sign in with google
+      </button>
       <h1 className="cursor-pointer text-slate-800" onClick={() => reg("Register")}>Don't have an account yet?</h1>
     </div>
   )
