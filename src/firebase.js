a8d889d193a939247e276039b13729e7c8e4bb5b
Jxk0be <jakeshoffner27@gmail.com>
2024-03-20 17:22:28 -0400

    env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPKlZhO3bwWT03Q6QjelQ_yyF_P1OPo-M",
-  authDomain: "nn-poker.firebaseapp.com",
-  projectId: "nn-poker",
-  storageBucket: "nn-poker.appspot.com",
-  messagingSenderId: "927627331489",
-  appId: "1:927627331489:web:cb081e385407e269756de0"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
