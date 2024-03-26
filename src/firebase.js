8dba8ba0ad897fed2f0bfa6614dfe731ddf834e3
Jxk0be <jakeshoffner27@gmail.com>
2024-03-25 23:01:25 -0400

    Adding a user if they don't exist already within the users collection

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -16,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,36 @@
 import PokerTableComponent from "../components/PokerTableComponent";
 import Navbar from "../components/Navbar";
+import { useEffect } from "react";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { auth, db } from "../firebase"
+import { onAuthStateChanged } from "firebase/auth";
 
 const Home = () => {
+
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const docRef = doc(db, "users", user.uid)
+        const docSnap = await getDoc(docRef)
+        let userData = {
+            email: user.email,
+            history: []
+        }
+
+        if (!docSnap.exists()) {
+            const userRef = doc(db, "users", user.uid)
+            await setDoc(
+                userRef,
+                userData,
+                { merge: true }
+            )
+        }
+      }
+
+      return () => listen()
+    })
+  }, [])
+  
   return (
     <div className='w-full h-screen md:pt-[70px] pt-[60px] flex justify-center items-center'>
         <Navbar />
