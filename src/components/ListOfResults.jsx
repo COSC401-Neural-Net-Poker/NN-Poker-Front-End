0f8050d2e1da3e18a0107a6a54939c29449ebd70
Jxk0be <jakeshoffner27@gmail.com>
2024-04-02 16:09:14 -0400

    active page refactor, waaaay better now

--- src/components/ListOfResults.jsx
+++ src/components/ListOfResults.jsx
@@ -5,13 +5,9 @@ import { Icon } from '@iconify/react';
 const ListOfResults = ({history = []}) => {
   let reversedHistory = []
   for (let i = history?.length - 1; i >= 0; --i) reversedHistory.push(history[i])
-  const [historyDisplay, setHistoryDisplay] = useState(reversedHistory.slice(0, 10))
 
-  const b1 = document.querySelector('.B1')
-  const b2 = document.querySelector('.B2')
-  const b3 = document.querySelector('.B3')
-  const b4 = document.querySelector('.B4')  
-  const b5 = document.querySelector('.B5')
+  const [historyDisplay, setHistoryDisplay] = useState(reversedHistory.slice(0, 10))
+  const [activePage, setActivePage] = useState(0);
 
   const pageButtons = []
   let counter = 1
@@ -25,89 +21,38 @@ const ListOfResults = ({history = []}) => {
   }
 
   const updateHistoryShown = (page) => {
-    if (page == 0) {
-      setHistoryDisplay(reversedHistory.slice(0, 10))
-      if (b1) {
-        b1.style.backgroundColor = "#4B4B4B";
-        b1.style.color = "#FF8200"
-      }      
-      if (b2) b2.style = null
-      if (b3) b3.style = null
-      if (b4) b4.style = null
-      if (b5) b5.style = null
-    }
-    if (page == 1) { 
-      setHistoryDisplay(reversedHistory.slice(10, 20))
-      if (b2) {
-        b2.style.backgroundColor = "#4B4B4B";
-        b2.style.color = "#FF8200"
-      }      
-      if (b1) b1.style = null
-      if (b3) b3.style = null
-      if (b4) b4.style = null
-      if (b5) b5.style = null
-    }
-    if (page == 2) {
-      setHistoryDisplay(reversedHistory.slice(20, 30))
-      if (b3) {
-        b3.style.backgroundColor = "#4B4B4B";
-        b3.style.color = "#FF8200"
-      }
-      if (b2) b2.style = null
-      if (b1) b1.style = null
-      if (b4) b4.style = null
-      if (b5) b5.style = null
-    }
-    if (page == 3) {
-      setHistoryDisplay(reversedHistory.slice(30, 40))
-      if (b4) {
-        b4.style.backgroundColor = "#4B4B4B";
-        b4.style.color = "#FF8200"
-      }      
-      if (b2) b2.style = null
-      if (b3) b3.style = null
-      if (b1) b1.style = null
-      if (b5) b5.style = null
-    }
-    if (page == 4) {
-      setHistoryDisplay(reversedHistory.slice(40, 50))
-      if (b5) {
-        b5.style.backgroundColor = "#4B4B4B";
-        b5.style.color = "#FF8200"
-      }
-      if (b2) b2.style = null
-      if (b3) b3.style = null
-      if (b4) b4.style = null
-      if (b1) b1.style = null
-    }
+    setActivePage(page)
+    setHistoryDisplay(reversedHistory.slice(page * 10, (page + 1) * 10))
   }
 
   return (
-    <div className="flex w-full items-center h-full flex-col">
-        {historyDisplay.map((game, ind) => {
-            return(
-              <Link key={ind} className='w-full h-[7%] my-1' to={`/history/${ind}`}>
-                <div className="h-full w-full cursor-pointer bg-[#cbcfcc] hover:bg-[#4B4B4B] hover:text-[#cbcfcc] duration-150 ease-in-out transition-all text-[#4B4B4B] flex justify-between items-center text-2xl md:text-3xl font-bold">
-                  <h1 className="px-3">{game.date}:</h1>
-                  <div className="flex items-center px-3">
-                    {game.result === "win" ? <Icon icon="mdi:crown" /> : <Icon icon="mage:robot-fill" />}
-                    <h1>{game.result}</h1>
+    <>
+      <div className="flex w-full items-center h-full flex-col">
+          {historyDisplay.map((game, ind) => {
+              return(
+                <Link key={ind} className='w-full h-[7%] my-1' to={`/history/${ind}`}>
+                  <div className="h-full w-full cursor-pointer bg-[#cbcfcc] hover:bg-[#4B4B4B] hover:text-[#cbcfcc] duration-150 ease-in-out transition-all text-[#4B4B4B] flex justify-between items-center text-2xl md:text-3xl font-bold">
+                    <h1 className="px-3">{game.date}:</h1>
+                    <div className="flex items-center px-3">
+                      {game.result === "win" ? <Icon icon="mdi:crown" /> : <Icon icon="mage:robot-fill" />}
+                      <h1>{game.result}</h1>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            )
-        })}
-        {pageButtons.length < 2 ? '' :
+                </Link>
+              )
+          })}
+      </div>
+
+      {pageButtons.length < 2 ? '' :
         <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 md:mb-[60px] mb-[10%] flex w-full justify-center items-center">
           {pageButtons.map((button, ind) => {
-            let classId = "B" + (ind + 1)
             return(
-              <button key={ind} onClick={() => updateHistoryShown(ind)} className={`${classId} bg-[#FF8200] mx-1 text-white font-bold py-3 px-6 text-lg rounded-lg`}>{button}</button>
+              <button key={ind} onClick={() => updateHistoryShown(ind)} className={`${activePage === ind ? 'bg-gray-800 text-orange-500' : ''} bg-[#FF8200] mx-1 text-white font-bold py-3 px-6 text-lg rounded-lg`}>{button}</button>
             )
           })}
         </div> 
-        }
-    </div>
+      }
+    </>
   )
 }
 
